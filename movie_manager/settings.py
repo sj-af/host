8c5c9e26b290e6e6dd190aea6e510ddc20492bb8
sj-af <pssoorajkbfc@gmail.com>
2024-12-28 07:07:24 +0530

    Update settings.py

--- movie_manager/settings.py
+++ movie_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sorbi#qrim7cd4suh+7$p-$3eol%on#1uu644&jdjrk+#+p20f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
